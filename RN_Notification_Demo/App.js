7684ab4039f7502220c26302f73739a1d233b47e
Enes Lisovac <lisovac.enes@gmail.com>
2018-03-23 16:28:30 +0100

    Fifth commit

--- RN_Notification_Demo/App.js
+++ RN_Notification_Demo/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <PushNotificationController />
         <Text style={styles.welcome}>
-          This is app received from Visual App Center! CI implemented fully!
+          This is app received from Visual App Center! CI implemented fully on push!
         </Text>
       </View>
     );
