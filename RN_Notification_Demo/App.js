3db0b0af3c6b112c75e23e0ce76de6ef6ecdb019
Enes Lisovac <lisovac.enes@gmail.com>
2018-03-23 15:51:05 +0100

    Second commit

--- RN_Notification_Demo/App.js
+++ RN_Notification_Demo/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <PushNotificationController />
         <Text style={styles.welcome}>
-          Welcome to Push Notification for React Native
+          This is app received from Visual App Center!
         </Text>
       </View>
     );
